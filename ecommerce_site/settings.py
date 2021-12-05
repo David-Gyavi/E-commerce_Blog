a1bbc4869af45fc2eeb3a639aff3d15d743c65fd
David-Gyavi <dbloosi12@gmail.com>
2021-12-05 07:53:55 +0000

    profile part 6 co mpleted confirmation emails

--- ecommerce_site/settings.py
+++ ecommerce_site/settings.py
@@ -179,6 +179,7 @@ STRIPE_CURRENCY = 'usd'
 STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', 'pk_test_51K2yXaJPAQhAIeGiK1RIbU4KvYonBvlUc73mX81p8sy9JbvtozqeBSFZXQ1mhIDCVV4mSXO2LQ3npNx5Qy5MrTDj00A3pmOW8f')
 STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', 'sk_test_51K2yXaJPAQhAIeGi4JAattJ8uyT1FhdlUNRWZx5x4FUXFnd898CcYqHFWRPxVXwedHWUZuG40fmyEVdrHZ1qabV300ZD5lnjyC')
 STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', 'whsec_Ze3qPKtv76vxh6NqscJo6DAr7M8kBCxl')
+DEFAULT_FROM_EMAIL = 'vken8936@gmail.com'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
