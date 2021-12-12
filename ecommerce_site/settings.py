0139569a31d9fc743dbd8d7399a4b8b55d929362
David-Gyavi <dbloosi12@gmail.com>
2021-12-12 03:02:51 +0000

    059 - deployment part 5

--- ecommerce_site/settings.py
+++ ecommerce_site/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', '')
+SECRET_KEY = 'z=o$!(s$_=n68!u=qb$h5rz6h$@zeam!cq-9e^!k8op36)()1='
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
