5b3adc862de092cbae1bb6d519a637ad9c934590
David-Gyavi <dbloosi12@gmail.com>
2022-01-24 21:39:46 +0000

    Deployment to heroku is done in this project

--- ecommerce_site/settings.py
+++ ecommerce_site/settings.py
@@ -125,17 +125,13 @@ WSGI_APPLICATION = 'ecommerce_site.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 
-if 'DATABASE_URL' in os.environ:
-    DATABASES = {
-        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
-    }
-else:
-    DATABASES = {
+DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.sqlite3',
             'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
         }
     }
+
 
 
 # Password validation
