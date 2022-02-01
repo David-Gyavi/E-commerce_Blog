1e608c2876613eec9ce451c02b17c227d9c8491f
David-Gyavi <dbloosi12@gmail.com>
2022-02-01 23:39:38 +0000

    real-email added to the project

--- ecommerce_site/settings.py
+++ ecommerce_site/settings.py
@@ -219,7 +219,7 @@ STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
 
 if 'DEVELOPMENT' in os.environ:
     EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-    DEFAULT_FROM_EMAIL = 'dwasswa694@gmail.com'
+    DEFAULT_FROM_EMAIL = 'naggynag644@gmail.com'
 else:
     EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
     EMAIL_USE_TLS = True
